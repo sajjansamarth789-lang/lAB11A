bd366b1a2f3aeeb39af45758d16b6bc5eab1a992
sajjansamarth789-lang <sajjansamarth789@gmail.com>
2026-04-19 16:36:22 +0530

    Update UserDataServlet.java

--- src/main/java/com/HTTP_REQ_RES/UserDataServlet.java
+++ src/main/java/com/HTTP_REQ_RES/UserDataServlet.java
@@ -1,4 +1,9 @@
- package com.HTTP_REQ_RES;
+//10.a Build an Application to get Username,Email and Designation through JSP called index.jsp with
+//client sided validation and submit to the servlet called UserDataServlet and process all the fields with
+//server sided validation and display all the data through result.jsp with a link to move to the client side
+
+
+package com.HTTP_REQ_RES;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
